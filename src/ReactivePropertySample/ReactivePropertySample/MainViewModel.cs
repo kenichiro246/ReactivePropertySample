af0d1329f247d21a171fb0ec1ea0effafdf90fbf
kenichiro246 <tomorrow.is.another.day.246@gmail.com>
2018-06-07 01:33:26 +0900

    add solution

---
+++ src/ReactivePropertySample/ReactivePropertySample/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reactive.Linq;
+using Reactive.Bindings;
+
+namespace ReactivePropertySample
+{
+    class MainViewModel
+    {
+        public ReactiveProperty<long> Counter { get; }
+        public ReactiveProperty<string> InputText { get; }
+        public ReactiveProperty<string> OutputText { get; }
+
+        public MainViewModel()
+        {
+            Counter = Observable.Interval(TimeSpan.FromSeconds(1)).ToReactiveProperty();
+
+            InputText = new ReactiveProperty<string>("");
+            OutputText= this.InputText
+                .Select(x => x.ToUpper())
+                .ToReactiveProperty();
+        }
+    }
+}
